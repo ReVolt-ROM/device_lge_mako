e4a21cc8d01af96727952f3b929a2dbe487d53ab
Robert Craig <rpcraig@tycho.ncsc.mil>
2013-05-22 06:42:48 -0400

    More SELinux policy related changes.
    * create a qmux_socket domain for talking with
      the qmuxd multiplexer.
    * add a new domain for a shell that is started
      by init using a UID other than shell.
    
    Change-Id: Idaaa1b8a36f6d8cd79f25fc66c59ede0bedd7900
    Signed-off-by: rpcraig <rpcraig@tycho.ncsc.mil>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -111,6 +111,7 @@ BOARD_SEPOLICY_DIRS += \
 
 BOARD_SEPOLICY_UNION += \
 	file_contexts \
+	te_macros \
 	bd_addr_loader.te \
 	bluetooth.te \
 	bridge.te \
@@ -122,7 +123,7 @@ BOARD_SEPOLICY_UNION += \
 	file.te \
 	kickstart.te \
 	init.te \
-	init_shell.te \
+	init_trusted_shell.te \
 	mediaserver.te \
 	mpdecision.te \
 	netmgrd.te \
