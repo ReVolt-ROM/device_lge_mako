432961fdd8ebaf7d38b1151c75f0e5829e74fc82
Robert Craig <rpcraig@tycho.ncsc.mil>
2013-05-28 06:12:33 -0400

    More SELinux policy changes.
    Change-Id: Ie145ed63d898c5f506ee25c8a22fb3e2379ef29c

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -112,7 +112,7 @@ BOARD_SEPOLICY_DIRS += \
 BOARD_SEPOLICY_UNION += \
 	file_contexts \
 	te_macros \
-	bd_addr_loader.te \
+	bluetooth_loader.te \
 	bluetooth.te \
 	bridge.te \
 	camera.te \
@@ -123,7 +123,6 @@ BOARD_SEPOLICY_UNION += \
 	file.te \
 	kickstart.te \
 	init.te \
-	init_trusted_shell.te \
 	mediaserver.te \
 	mpdecision.te \
 	netmgrd.te \
@@ -133,6 +132,7 @@ BOARD_SEPOLICY_UNION += \
 	sensors.te \
 	surfaceflinger.te \
 	system.te \
+	tee.te \
 	thermald.te \
 	ueventd.te \
 	wpa_supplicant.te
