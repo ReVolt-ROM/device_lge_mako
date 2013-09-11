bb568754dcec748898b2c3381ca9f2e485203dc6
Lars Greiss <kufikugel@googlemail.com>
2013-09-11 10:56:04 +0200

    mako: do not use global cpp flag
    at least the result is the same but let us user the
    propper way like we do it with all flags and do not
    set if over a global cpp flag
    
    Change-Id: I1b7a229226af96cdfd294752823939a2cf870649

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -15,7 +15,7 @@
 #
 
 TARGET_GLOBAL_CFLAGS += -mfpu=neon -mfloat-abi=softfp
-TARGET_GLOBAL_CPPFLAGS += -mfpu=neon -mfloat-abi=softfp -DTARGET_MAKO_JWR66
+TARGET_GLOBAL_CPPFLAGS += -mfpu=neon -mfloat-abi=softfp
 TARGET_CPU_ABI := armeabi-v7a
 TARGET_CPU_ABI2 := armeabi
 TARGET_CPU_SMP := true
@@ -35,6 +35,9 @@ TARGET_KRAIT_BIONIC_PLDSIZE := 64
 
 TARGET_NO_BOOTLOADER := true
 TARGET_KERNEL_CONFIG := mako_defconfig
+
+# Use JWR66Y kernel compatibility fix
+TARGET_USE_JWR66Y := true
 
 BOARD_KERNEL_BASE := 0x80200000
 BOARD_KERNEL_PAGESIZE := 2048
