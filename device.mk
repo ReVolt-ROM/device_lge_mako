3f219dc616fd62e0bedee77e0aef521c86f1201e
John Hany <johnhany97@gmail.com>
2013-11-20 17:34:17 -0500

    Revert "CAF cleaning"
    This reverts commit 5bc8079968229d35b241616c3602805ce658cbc1.
    
    Conflicts:
    
    	device.mk

--- device.mk
+++ device.mk
@@ -267,8 +267,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # QRNGD
 PRODUCT_PACKAGES += qrngd
 
-$(call inherit-product, frameworks/native/build/phone-xhdpi-2048-dalvik-heap.mk)
-
 # This is the mako-specific audio package
 $(call inherit-product, frameworks/base/data/sounds/AudioPackage10.mk)
 
